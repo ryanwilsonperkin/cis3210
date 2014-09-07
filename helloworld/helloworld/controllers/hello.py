96effd39d1dc3d74fa21dc6331d30fc7ccbe48a2
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-09-07 14:45:45 -0400

    Create mako template and render in controller.

--- helloworld/helloworld/controllers/hello.py
+++ helloworld/helloworld/controllers/hello.py
@@ -10,7 +10,4 @@ log = logging.getLogger(__name__)
 class HelloController(BaseController):
 
     def index(self):
-        # Return a rendered template
-        #return render('/hello.mako')
-        # or, return a string
-        return 'Hello World'
+        return render('/hello.mako')
