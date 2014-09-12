d34b4a0930275879aa3a68390ee22ee762f65746
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-09-11 20:04:03 -0400

    Drop-down list of animations. Textarea for speech.

--- helloworld/helloworld/public/js/app.js
+++ helloworld/helloworld/public/js/app.js
@@ -5,13 +5,21 @@ var linksAgent;
 clippy.load('Links', function(agent) {
     linksAgent = agent;
     agent.show();
-});
 
-/* Register actions to buttons. */
-$('#animate').click(function() {
-    linksAgent.animate();
-});
+    /* Register actions to select dropdown. */
+    $.each(linksAgent.animations(), function(index, animation) {
+        $('#animation-list').append(
+            $('<option></option>').val(animation).html(animation)
+        );
+    });
 
-$('#say-hello').click(function() {
-    linksAgent.speak('Hello!');
+    /* Trigger play on click of animate button. */
+    $('#animate').click(function() {
+        linksAgent.play($('#animation-list').val());
+    });
+
+    /* Trigger speaking on click of say-hello button. */
+    $('#say-hello').click(function() {
+        linksAgent.speak($('#links-text').val());
+    });
 });
