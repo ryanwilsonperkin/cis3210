aa9c3c61af02cbf931ee6f7fc58c9b0cf9bb372d
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-09-19 11:19:40 -0400

    Complete functionality of retrieving koans.

--- lab/lab/public/js/app.js
+++ lab/lab/public/js/app.js
@@ -1,7 +1,42 @@
-$(function() {
-    $.getJSON('/koans', function(koansList) {
-        $.each(koansList, function(index, value) {
-            $('#koanSelector').append($('<option></option>').text(value));
+var g_koansList;
+
+// Fetch koan from server and insert it on the page.
+function fetchKoan(id) {
+    koanDiv = $('#koan');
+    $.getJSON('/koans/' + id, function(koan) {
+        // Clear away old koan.
+        koanDiv.empty();
+
+        // Insert new koan title.
+        koanDiv.append($('<h2></h2>').text(koan.title));
+
+        // Insert new koan text.
+        $.each(koan.text, function(index, val) {
+            koanDiv.append($('<p></p>').text(val));
         });
+    });
+};
+
+$(document).ready(function() {
+    // Populate koansList.
+    $.getJSON('/koans', function(koansList) {
+        g_koansList = koansList;
+        $.each(koansList, function(index, val) {
+            $('#koanSelector').append($('<option></option>').text(val));
+        });
+    });
+
+    // Add functionality to fetchKoan.
+    $('#fetchKoan').click(function(e) {
+        // Get index of koan from value of koanSelector.
+        index = g_koansList.indexOf($('#koanSelector').val());
+        fetchKoan(index);
+    });
+
+    // Add functionality to fetchRandomKoan.
+    $('#fetchRandomKoan').click(function(e) {
+        // Get random index within range.
+        index = Math.floor(Math.random() * g_koansList.length);
+        fetchKoan(index);
     });
 });
