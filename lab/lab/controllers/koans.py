9b60ffca7f6932772036b08682ab2e313c8a73f6
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-09-19 19:19:19 -0400

    Handle RESTful requests with no implementation.

--- lab/lab/controllers/koans.py
+++ lab/lab/controllers/koans.py
@@ -12,12 +12,17 @@ log = logging.getLogger(__name__)
 class KoansController(BaseController):
 
     def index(self):
-        """GET /koans"""
+        """GET /koans: fetch list of koans."""
+        if request.method != 'GET':
+            abort(405)
         response.headers['Content-Type'] = 'text/javascript'
         return json.dumps(koan_dict.keys())
 
     def show(self, id):
-        """GET /koans/id"""
+        """GET /koans/id: access koan at index id."""
+        if request.method != 'GET':
+            abort(405)
+
         if id.isdigit() and 0 <= int(id) < len(koan_dict):
             id = int(id)
         else:
@@ -30,3 +35,39 @@ class KoansController(BaseController):
             "title": title,
             "text": text
         })
+
+    # Unimplemented API endpoints due to lack of database.
+    def create(self):
+        """POST /koans: create a new koan."""
+        if request.method != 'POST':
+            abort(405)
+        else:
+            abort(403, comment='Method not yet implemented.')
+
+    def new(self):
+        """POST /koans/new: form for creating a new koan."""
+        if request.method != 'POST':
+            abort(405)
+        else:
+            abort(403, comment='Method not yet implemented.')
+
+    def update(self, id):
+        """PUT /koans/id: update existing koan at index id."""
+        if request.method != 'PUT':
+            abort(405)
+        else:
+            abort(403, comment='Method not yet implemented.')
+
+    def delete(self, id):
+        """DELETE /koans/id: delete koan at index id."""
+        if request.method != 'DELETE':
+            abort(405)
+        else:
+            abort(403, comment='Method not yet implemented.')
+
+    def edit(self, id):
+        """GET /koans/id: form to edit koan at index id."""
+        if request.method != 'GET':
+            abort(405)
+        else:
+            abort(403, comment='Method not yet implemented.')
