c2503ee551eb9cc73b8b0b65ef6be70176714fbc
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-09-26 00:59:13 -0400

    Nose test for index and show.

--- lab/lab/tests/functional/test_koans.py
+++ lab/lab/tests/functional/test_koans.py
@@ -1,7 +1,25 @@
+import json
+
 from lab.tests import *
+from lab.model.koans import koan_dict
+from lab.controllers.koans import not_implemented_msg
 
 class TestKoansController(TestController):
 
     def test_index(self):
-        response = self.app.get(url('koans'))
-        # Test response...
+        response = self.app.get(url(controller='koans', action='index'))
+
+        assert response.status_code == 200
+        assert response.content_type == 'text/javascript'
+        assert response.json_body == koan_dict.keys()
+
+    def test_show(self):
+        response = self.app.get(url(controller='koans', action='show', id=0))
+
+        assert response.status_code == 200
+        assert response.content_type == 'text/javascript'
+
+        koan_title, koan_text = koan_dict.items()[0]
+        assert response.json_body.get('title') == koan_title
+        assert response.json_body.get('text') == koan_text
+
