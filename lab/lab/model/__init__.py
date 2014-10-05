7f16ff87a9ffcfd701282482f85d385d192ef43a
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-10-05 12:09:55 -0400

    Re-configure project for SQLAlchemy setup.

--- lab/lab/model/__init__.py
+++ lab/lab/model/__init__.py
@@ -1,5 +1,7 @@
+"""The application's model objects"""
 from lab.model.meta import Session, Base
 from lab.model.user import User
 
 def init_model(engine):
+    """Call me before using any of the tables or classes in the model"""
     Session.configure(bind=engine)
