1bf94da71499169c63c82e60d9b217e718060f2e
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-11-03 10:52:36 -0500

    Append meetups to page.

--- lab/lab/public/js/app.js
+++ lab/lab/public/js/app.js
@@ -1,8 +1,30 @@
-function get_meetups(text) {
-    url = '/meetup/groups/' + text;
-    return $.getJSON(url, function(data) {
-        $.each(data, function(index, meetup) {
-            // Add meetup to page.
-        });
+/*global $, jQuery*/
+function append_meetup(meetup) {
+    var fields = ['city', 'link', 'lat', 'lon', 'members', 'name', 'who'];
+    var $ul = $('<ul>');
+    $.each(fields, function() {
+        var $li = $('<li>');
+        $li.append($('<strong>').text(this + ': '));
+        $li.append($('<span>').text(meetup[this]));
+        $ul.append($li);
     });
+    $('#meetups').append($ul);
+}
+
+function get_meetups(text) {
+    var url = '/meetup/groups/' + text;
+    $.getJSON(url)
+        .success(function(data) {
+            $('#meetups').empty();
+            $.each(data, function() {
+                append_meetup(this);
+            });
+        })
+        .fail(function() {
+            var $error = $('<div>');
+            $error.addClass('alert alert-warning');
+            $error.attr('role', 'alert');
+            $error.text('error: Could not fetch data from api.meetup.com');
+            $('#meetups').append($error);
+        });
 }
